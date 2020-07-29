93d85d498d9170dc4fb7ffdb9ec2debb4691f370
Anubhav <anubhav.workemail@gmail.com>
2020-07-29 06:53:41 +0530

    adding the dummy data

--- src/components/EventsComponent.vue
+++ src/components/EventsComponent.vue
@@ -1,7 +1,10 @@
 <template>
 <div>
-    <b-table striped hover small responsive=true stacked="sm" :items="usersList" :fields="usersListFields"></b-table>
-    
+       <h3>Ongoing</h3>
+    <b-table striped hover small responsive=true stacked="sm" :items="usersList"></b-table>
+    <h3>Up Coming</h3>
+     <b-table striped hover small responsive=true stacked="sm" :items="upcoming"></b-table>
+
 </div>    
 </template>
 
@@ -16,18 +19,76 @@
         data(){
             return{
                 usersList:[],
-                 usersListFields:['title','description','users', 'tags', 'scheduledOn', 'createdBy']
+                upcoming : [],
+                usersListBase : []
               }
         },
         methods: {
             search(){
-               this.usersList.push({"title":"Lets play mafia","description":"Its weekend coming, lets play :)","users":["Anubhav Shrivastava"," ,Shakun Grover"],"tags":["gaming"],"createdBy":"Anubhav Shrivastava","scheduledOn":"28/07/20 01:10:15 - pm"});
-               this.usersList.push({"title":"Chess anyone?","description":"Have been so long :(","users":["Ajay Suri"],"tags":["entertainment"],"createdBy":"Ajay Suri","scheduledOn":"30/07/20 10:10:15 - pm"});
-               this.usersList.push({"title":"Toastmasters","description":"Learn public speaking & shed your inhibitions","users":["Siddhant Bhatt"],"tags":["public speaking"],"createdBy":"Siddhant Bhatt","scheduledOn":"01/08/20 01:10:15 - pm"});
+                   for(var x= 0; x< this.usersListBase.length; x++){
+                         var user  = {};
+                         user.Title = this.usersListBase[x].title;
+                         user.Description = this.usersListBase[x].description;
+                         user.Participants = "";
+                         for(var i=0;i< this.usersListBase[x].users.length ; i++){
+                                user.Participants = user.Participants + this.usersListBase[x].users[i].name;
+                                if(i+1!= this.usersListBase[x].users.length){
+                                       user.Participants = user.Participants + ", ";
+                                }
+                         }
+                         
+                         user.tags = "";
+                         for( var i=0; i< this.usersListBase[x].tags.length; i++){
+                                user.tags = user.tags + this.usersListBase[x].tags[i];
+                                if(i+1!= this.usersListBase[x].tags.length){
+                                       user.tags = user.tags+ ", ";
+                                }
+                         }
+                         user.Oraganizer = this.usersListBase[x].createdBy.name;
+                         
+                  
+                         console.log("date found  = "+ this.usersListBase[x].scheduledOn);
 
+                         
+
+                         var EventStartTime  = new Date(this.usersListBase[x].scheduledOn);
+                         
+                         var currentTime = new Date();
+                         console.log(EventStartTime.getTime());
+                         console.log(currentTime.getTime());
+                         console.log(EventStartTime.getTime() + parseInt(this.usersListBase[x].timeInMunutes) *60000);
+
+                         var start = parseInt(EventStartTime.getTime());
+                         var end = parseInt(EventStartTime.getTime() + parseInt(this.usersListBase[x].timeInMunutes) *60000);
+                         var current = new Date().getTime();
+
+
+
+                         if(end < current){
+                                // past event
+                         }
+
+                         if(start < current && end > current ){
+                                this.usersList.push(user);  // ongoing
+                         }
+
+                         else{
+                                this.upcoming.push(user);
+                         }
+
+                         user.Time = EventStartTime.toDateString() + " " + EventStartTime.toLocaleTimeString();
+
+                   }
             }
         },
         created:function () {
+               this.usersListBase.push({"title":"Lets play mafia","description":"Its weekend coming, lets play :)","users":[{"email":"shgrover@microsoft.com","name":"Shakun grover"},{"email":"anshriv@microsoft.com","name":"Anubhav Shrivastava"}],"tags":["gaming"],"createdBy":{"email":"anshriv@microsoft.com","name":"Anubhav Shrivastava"},"scheduledOn":"29 Jul 2020 00:30:00 GMT","timeInMunutes":"60"});
+               this.usersListBase.push({"title":"Chess anyone?","description":"Have been so long :(, lets play :)","users":[{"email":"ajays@microsoft.com","name":"Ajay Suri"}],"tags":["gaming"],"createdBy":{"email":"ajays@microsoft.com","name":"Ajay Suri"},"scheduledOn":"30 Jul 2020 00:30:00 GMT","timeInMunutes":"60"});
+               this.usersListBase.push({"title":"Toastmasters","description":"Learn public speaking & shed your inhibitions","users":[{"email":"siddhant.bhatt@microsoft.com","name":"Siddhant Bhatt"},{"email":"anshriv@microsoft.com","name":"Anubhav Shrivastava"}],"tags":["gaming"],"createdBy":{"email":"siddhant.bhatt@microsoft.com","name":"Siddhant Bhatt"},"scheduledOn":"29 Aug 2020 00:30:00 GMT","timeInMunutes":"60"});
+ //              this.usersListBase.push({"title":"Chess anyone?","description":"Have been so long :(","users":["Ajay Suri"],"tags":["entertainment"],"createdBy":"Ajay Suri","scheduledOn":"30/07/20 10:10:15 - pm"});
+ //              this.usersListBase.push({"title":"Toastmasters","description":"Learn public speaking & shed your inhibitions","users":["Siddhant Bhatt"],"tags":["public speaking"],"createdBy":"Siddhant Bhatt","scheduledOn":"01/08/20 01:10:15 - pm"});
+
+console.log(this.usersListBase);
          this.search();
         }
     }
